b21da3b029821b1df41fd09fb1f4f514df3121f4
jacquardlabs <66028282+jacquardlabs@users.noreply.github.com>
2021-12-02 17:10:19 -0600

    Skeleton problem

---
+++ src/year2021/Day2021_.kt
@@ -0,0 +1,16 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input
+            .size
+
+    }
+
+    fun part2(input: List<String>): Int {
+        return input
+            .size
+    }
+
+    val input = readInput("2021/day1")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
